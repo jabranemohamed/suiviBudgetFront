3aa71c08e5f01eba614c0b845f2cc06b32d50800
mhjabrane <mohamed.jabrane@adservio.fr>
2019-12-20 15:43:35 +0100

    Ajouter la composante IHM de recherche Budget

--- src/app/components/admin/budgets/budgets.component.ts
+++ src/app/components/admin/budgets/budgets.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit  } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators, ValidationErrors } from '@angular/forms';
+import { Observable, Observer } from 'rxjs';
 
 @Component({
   selector: 'app-budgets',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BudgetsComponent implements OnInit {
 
-  constructor() { }
+  budgetValidationForm: FormGroup;
+
+  constructor(  private resgisterFB: FormBuilder) { }
 
   ngOnInit() {
+
+    this.budgetValidationForm = this.resgisterFB.group({
+      codeUL        : [ null, [ Validators.required ] ],
+      datePicker    : [ null, [ Validators.required ] ],
+    });
+  }
+
+  registerSubmitForm(): void {
+    for (const i in this.budgetValidationForm.controls) {
+      this.budgetValidationForm.controls[ i ].markAsDirty();
+      this.budgetValidationForm.controls[ i ].updateValueAndValidity();
+    }
   }
 
 }
--- src/app/components/admin/components-admin.module.ts
+++ src/app/components/admin/components-admin.module.ts
@@ -1,6 +1,6 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from "@angular/common";
-import {FormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {SharedModule} from '../../shared/shared.module';
 import {HighlightModule} from 'ngx-highlightjs';
 import {InfiniteScrollModule} from 'ngx-infinite-scroll';
@@ -15,24 +15,29 @@ import {CentresComponent} from "./centres/centres.component";
 import {FilesUploadComponent} from "./files-upload/files-upload.component";
 import {RolesComponent} from "./roles/roles.component";
 import {UtilisateursComponent} from "./utilisateurs/utilisateurs.component";
-
+import {DatePickerComponent} from "../data-entry/date-picker/date-picker.component";
+import {TimePickerComponent} from "../data-entry/time-picker/time-picker.component";
 
 @NgModule({
-  imports: [
-    CommonModule,
-    SharedModule,
-    FormsModule,
-    HighlightModule,
-    ComponentsAdminRoutingModule,
-    InfiniteScrollModule
-  ],
+    imports: [
+        CommonModule,
+        SharedModule,
+        FormsModule,
+        HighlightModule,
+        ComponentsAdminRoutingModule,
+        InfiniteScrollModule,
+        ReactiveFormsModule,
+
+    ],
   exports: [],
   declarations: [
     BudgetsComponent,
     CentresComponent,
     FilesUploadComponent,
     RolesComponent,
-    UtilisateursComponent
+    UtilisateursComponent,
+    DatePickerComponent,
+    TimePickerComponent
   ],
   providers: [RandomUserService,
              TableService],
