aa4b061781b4e5f55c8815c10e55ca8e3e9bc03e
mhjabrane <mohamed.jabrane@adservio.fr>
2019-12-26 19:11:06 +0100

    Ajouter la recherche

--- src/app/components/admin/budgets/budgets.component.ts
+++ src/app/components/admin/budgets/budgets.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit  } from '@angular/core';
-import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators, ValidationErrors } from '@angular/forms';
-import { Observable, Observer } from 'rxjs';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {BudgetService} from "../../../shared/services/budget.service";
+import {TableService} from "../../../shared/services/table.service";
 
 @Component({
   selector: 'app-budgets',
@@ -11,22 +12,72 @@ export class BudgetsComponent implements OnInit {
 
   budgetValidationForm: FormGroup;
   datePicker: any;
+  codeUL: any;
+  displayData = [];
+  dataFound: Boolean;
+  noDataFoundMessageDisplay: Boolean;
+  allChecked: boolean = false;
+  indeterminate: boolean = false;
+  search: any;
 
-  constructor(  private resgisterFB: FormBuilder) { }
+  constructor(private resgisterFB: FormBuilder, private budgetService: BudgetService, public tableSvc: TableService) {
+  }
 
   ngOnInit() {
-
+    this.dataFound = false;
+    this.noDataFoundMessageDisplay = false;
     this.budgetValidationForm = this.resgisterFB.group({
-      codeUL        : [ null, [ Validators.required ] ],
-      datePicker    : [ null, [ Validators.required ] ],
+      codeUL: [null, [Validators.required]],
+      datePicker: [null, [Validators.required]]
     });
   }
 
   registerSubmitForm(): void {
+
     for (const i in this.budgetValidationForm.controls) {
-      this.budgetValidationForm.controls[ i ].markAsDirty();
-      this.budgetValidationForm.controls[ i ].updateValueAndValidity();
+      this.budgetValidationForm.controls[i].markAsDirty();
+      this.budgetValidationForm.controls[i].updateValueAndValidity();
     }
+    let cu = this.budgetValidationForm.get('codeUL').value;
+    let dp = this.budgetValidationForm.get('datePicker').value;
+    let fullYear = dp.getFullYear();
+    this.budgetService.findAllBudgetPerYearAndCodeUnit(fullYear, cu).subscribe(data => {
+      this.displayData = data.content;
+      if (this.displayData == null || this.displayData.length == 0) {
+        this.noDataFoundMessageDisplay = true
+      } else {
+        this.noDataFoundMessageDisplay = false;
+        this.dataFound = true;
+      }
+    });
+  }
+
+
+  refreshStatus(): void {
+    const allChecked = this.displayData.every(value => value.checked === true);
+    const allUnChecked = this.displayData.every(value => !value.checked);
+    this.allChecked = allChecked;
+    this.indeterminate = (!allChecked) && (!allUnChecked);
+  }
+
+  currentPageDataChange($event: Array<{
+    id: number;
+    libelle: string;
+    description: string;
+  }>): void {
+    this.displayData = $event;
+    this.refreshStatus();
+  }
+
+  sort(sortAttribute: any) {
+    this.displayData = this.tableSvc.sort(sortAttribute, this.displayData);
+  }
+
+  checkAll(value: boolean): void {
+    this.displayData.forEach(data => {
+      data.checked = value;
+    });
+    this.refreshStatus();
   }
 
 }
--- src/app/shared/services/budget.service.ts
+++ src/app/shared/services/budget.service.ts
@@ -1,9 +1,24 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {environment} from "../../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class BudgetService {
 
-  constructor() { }
+  token: String;
+  headers: HttpHeaders;
+  public API_URL = environment.baseUrl;
+
+  constructor(private http: HttpClient) {
+    this.token = sessionStorage.getItem('token');
+    this.headers = new HttpHeaders({'Content-Type': 'application/json'});
+    this.headers = this.headers.append('Authorization', 'Bearer ' + this.token);
+  }
+
+  findAllBudgetPerYearAndCodeUnit(fullYear: number, cu: any): Observable<any> {
+    return this.http.get(this.API_URL + "budgets/"+cu+"/"+fullYear , {headers: this.headers});
+  }
 }
